2931f33a8b2d7eeeb1f2c150ec7520ebf87837d8
Azenga Kevin <toomuch573@gmail.com>
2019-03-23 11:20:29 +0300

    Still thinking of a better way for implement the layouts but it is a basic blog Android/Kotlin App that includes the best features I can think of in a blog app

---
+++ app/src/main/java/com/shadow/bountiful/AddPost.kt
@@ -0,0 +1,12 @@
+package com.shadow.bountiful
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPost : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_add_post)
+    }
+}
--- app/src/main/java/com/shadow/bountiful/MainActivity.kt
+++ app/src/main/java/com/shadow/bountiful/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.shadow.bountiful
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addPostActivityFAB.setOnClickListener {
+            val intent = Intent(this, AddPost::class.java)
+            startActivity(intent)
+        }
     }
 }
